cae87048be42c120b12ad6d61e9a5e195dbbfb5f
David <3173957+0ex-d@users.noreply.github.com>
2026-07-20 22:14:00 +0100

    tiny change

--- app/android/app/src/main/java/com/localghost/app/debug/CrashHandler.kt
+++ app/android/app/src/main/java/com/localghost/app/debug/CrashHandler.kt
@@ -16,7 +16,6 @@ object CrashHandler {
         val appCtx = ctx.applicationContext
         val previous = Thread.getDefaultUncaughtExceptionHandler()
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            Log.e("LocalGhost", "uncaught exception on ${thread.name}", throwable)
             try { write(appCtx, thread, throwable) } catch (_: Throwable) {}
             previous?.uncaughtException(thread, throwable)
         }
